0ebfd18ae20c08b6c358b2844b5c5fa481001ae8
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-11 23:25:01 -0800

    Retry last commit

---
+++ plugins/quizbook/quizbook.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ Plugin Name: Quizbook
+ Plugin URI:
+ Description: Plugin for adding quizzes or questionnaires in WordPress.
+ Version: 1.0
+ Author: Gabriel S Valdivia
+ URL: https://portfolio.gabeswebcoding.com
+ License: GPLv3
+ License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+ Text Domain: quizbook
+*/
