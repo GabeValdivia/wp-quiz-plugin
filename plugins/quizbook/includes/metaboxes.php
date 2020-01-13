3e8fcf9d4f4bbc6375c6614b0b064309e7205319
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-12 20:29:53 -0800

    Add function to save values, add function to save selected correct answer

--- plugins/quizbook/includes/metaboxes.php
+++ plugins/quizbook/includes/metaboxes.php
@@ -13,7 +13,7 @@ add_action('add_meta_boxes', 'quizbook_agregar_metaboxes');
 * Add HTML content to metabox
 */
 
-function quizbook_metaboxes() { ?>
+function quizbook_metaboxes($post) { ?>
     <table>
         <tr>
             <th class="row-title">
@@ -25,7 +25,7 @@ function quizbook_metaboxes() { ?>
                 <label for="question_1">a)</label>
             </th>
             <td>
-                <input type="text" id="question_1" name="qb_question_1" class="regular-text">
+                <input value="<?php echo get_post_meta( $post->ID, 'qb_question_1', true ); ?>" type="text" id="question_1" name="qb_question_1" class="regular-text">
             </td>
         </tr>
         <tr>
@@ -33,7 +33,7 @@ function quizbook_metaboxes() { ?>
                 <label for="question_2">b)</label>
             </th>
             <td>
-                <input type="text" id="question_2" name="qb_question_2" class="regular-text">
+                <input value="<?php echo get_post_meta( $post->ID, 'qb_question_2', true ); ?>" type="text" id="question_2" name="qb_question_2" class="regular-text">
             </td>
         </tr>
         <tr>
@@ -41,7 +41,7 @@ function quizbook_metaboxes() { ?>
                 <label for="question_3">c)</label>
             </th>
             <td>
-                <input type="text" id="question_3" name="qb_question_3" class="regular-text">
+                <input value="<?php echo get_post_meta( $post->ID, 'qb_question_3', true ); ?>" type="text" id="question_3" name="qb_question_3" class="regular-text">
             </td>
         </tr>
         <tr>
@@ -49,7 +49,7 @@ function quizbook_metaboxes() { ?>
                 <label for="question_4">d)</label>
             </th>
             <td>
-                <input type="text" id="question_4" name="qb_question_4" class="regular-text">
+                <input value="<?php echo get_post_meta( $post->ID, 'qb_question_4', true ); ?>" type="text" id="question_4" name="qb_question_4" class="regular-text">
             </td>
         </tr>
         <tr>
@@ -57,7 +57,7 @@ function quizbook_metaboxes() { ?>
                 <label for="question_5">e)</label>
             </th>
             <td>
-                <input type="text" id="question_5" name="qb_question_5" class="regular-text">
+                <input value="<?php echo get_post_meta( $post->ID, 'qb_question_5', true ); ?>" type="text" id="question_5" name="qb_question_5" class="regular-text">
             </td>
         </tr>
         <tr>
@@ -65,13 +65,14 @@ function quizbook_metaboxes() { ?>
                 <label for="correct_answer">Correct Answer</label>
             </th>
             <td>
+                <?php $correct = get_post_meta( $post->ID, 'correct_answer', true ); ?>
                 <select id="correct_answer" name="correct_answer" class="postbox">
                     <option value="">Choose the correct answer</option>
-                    <option value="qb_correct:a">a</option>
-                    <option value="qb_correct:b">b</option>
-                    <option value="qb_correct:c">c</option>
-                    <option value="qb_correct:d">d</option>
-                    <option value="qb_correct:e">e</option>
+                    <option <?php selected($correct, 'qb_correct:a'); ?> value="qb_correct:a">a</option>
+                    <option <?php selected($correct, 'qb_correct:b'); ?> value="qb_correct:b">b</option>
+                    <option <?php selected($correct, 'qb_correct:c'); ?> value="qb_correct:c">c</option>
+                    <option <?php selected($correct, 'qb_correct:d'); ?> value="qb_correct:d">d</option>
+                    <option <?php selected($correct, 'qb_correct:e'); ?> value="qb_correct:e">e</option>
                 </select>
             </td>
         </tr>
