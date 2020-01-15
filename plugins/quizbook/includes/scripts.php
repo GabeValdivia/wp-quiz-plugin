62a48c07c5e05e75326d52e3193ac0e5f6b98da0
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-14 18:21:16 -0800

    Add stylesheet for admin-quizbook.css. Add if statement so that admin-quizbook.css only runs on quizes post_type

--- plugins/quizbook/includes/scripts.php
+++ plugins/quizbook/includes/scripts.php
@@ -1,6 +1,23 @@
 <?php 
 
+// Styles and scripts for the front end display
+
 function quizbook_frontend_styles(){
     wp_enqueue_style( 'quizbook_css', plugins_url('../assets/css/quizbook.css' ,  __FILE__ ) );
 }
 add_action('wp_enqueue_scripts', 'quizbook_frontend_styles');
+
+// Sytles and JS on admin side of quiz
+
+function quizbook_admin_styles( $hook ) {
+
+    global $post;
+    if($hook == 'post-new.php' || $hook == 'post.php') {
+        if($post->post_type === 'quizes'){
+            wp_enqueue_style( 'quizbookcss', plugins_url( '../assets/css/admin-quizbook.css', __FILE__ ) );
+        }
+        
+    }
+    
+}
+add_action('admin_enqueue_scripts', 'quizbook_admin_styles');
