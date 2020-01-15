7b707b446c9f8383a2835dabe923153960540545
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-14 19:06:02 -0800

    Write jQuery to add data-selected class to selected answer, remove if another answer is selected

--- plugins/quizbook/assets/js/quizbook.js
+++ plugins/quizbook/assets/js/quizbook.js
@@ -1,3 +1,8 @@
 (function($) {
-    
+    $('#quizbook ul li .answer').on('click', function(){
+        $(this).siblings().removeAttr('data-selected');
+        $(this).siblings().removeClass('selected');
+        $(this).addClass('selected');
+        $(this).attr('data-selected', true);
+    });
 })(jQuery);
