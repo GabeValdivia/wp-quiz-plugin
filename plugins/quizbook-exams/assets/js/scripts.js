14ce41d2cb4206cf36a1ced9be1e5f0db27b1c28
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-18 13:50:26 -0800

    Add ChosenJS to metaboxes.php. Test

--- plugins/quizbook-exams/assets/js/scripts.js
+++ plugins/quizbook-exams/assets/js/scripts.js
@@ -1,3 +1,3 @@
 (function($) {
-    console.log("It works!");
+    $('.questions_select').chosen();
 })(jQuery);
--- plugins/quizbook-exams/includes/metaboxes.php
+++ plugins/quizbook-exams/includes/metaboxes.php
@@ -11,7 +11,47 @@ add_action('add_meta_boxes', 'quizbook_exams_add_metaboxes');
 
 function quizbook_exams_metaboxes() { ?>
 
-    
+    <table class="form-table">
+        <tr>
+            <th class="row-title" colspan="2">
+                <h2>Select the questions you'd like to include in this exam</h2>
+            </th>
+
+        </tr>
+        <tr>
+            <th class="row-title">
+                <label for="">Select from the list</label>
+                <td>
+                    <?php 
+                        $args = array(
+                            'post_type' => 'quizes',
+                            'posts_per_page' => -1,
+                        );
+                        $questions = get_posts($args);
+                        
+                        if($questions):
+                        ?>
+                            <select data-placeholder="Choose a Question..." class="questions_select" multiple tabindex="4">
+                                <option value=""></option>
+                                <?php 
+                                    foreach ($questions as $question): ?>
+                                    <option value="<?php echo $question->ID; ?>"><?php echo $question->post_title; ?></option>
+                                <?php endforeach; ?>
+                                    
+                            </select>
+
+                        <?php
+                        else:
+                            echo "<p>You need to add questions via Quize menu option on the left menu.</p>";
+                        endif;
+                    
+                    ?>
+
+                </td>
+            </th>
+
+        </tr>
+    </table>
 
 <?php
 }
