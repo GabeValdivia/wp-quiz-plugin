5ad914f12240ad92c08b8f3a2bcaa7ad2b82ee85
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-18 10:56:40 -0800

    Create /quizbook-exams/quizbook_exams.php folder and file.

---
+++ plugins/quizbook-exams/quizbook_exams.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ Plugin Name: Quizbook Exams
+ Plugin URI:
+ Description: Extends Quizbook so that you can create custom exams in your project. (Add-on)
+ Version: 1.0
+ Author: Gabriel S Valdivia
+ URL: https://portfolio.gabeswebcoding.com
+ License: GPLv3
+ License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+ Text Domain: quizbook
+*/
+
